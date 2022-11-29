2ea6f38a8601b34ef27e5b8e208e76a73547fe50
Taishi Hashimoto <hashimoto.taishi@outlook.com>
2022-11-29 17:10:40 +0900

    feat: example of computing means of selected X, Y

---
+++ examples/mean.py
@@ -0,0 +1,25 @@
+"Compute mean of selected X and Y coordinates."
+import matplotlib.pyplot as plt
+from mpl_lassotool import LassoTool, EventHandler
+import numpy as np
+
+
+class CustomEventHandler(EventHandler):
+    "For each artist, mean of selected X and Y are computed."
+    def on_close(self, lt: LassoTool):
+        data = super().on_close(lt)
+        for artist, index in data.items():
+            print(artist)
+            x, y = self.get_xy_from_artist(artist)
+            print("mean X: {:.3g}, mean Y: {:.3g}".format(
+                np.mean(x[index]), np.mean(y[index])
+            ))
+
+
+fig, ax = plt.subplots()
+
+data = np.random.normal(size=(2, 10000))
+ax.scatter(*data)
+
+_ = LassoTool(fig, CustomEventHandler())
+plt.show()
